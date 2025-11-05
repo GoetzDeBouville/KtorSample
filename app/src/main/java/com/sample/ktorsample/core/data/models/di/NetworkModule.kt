8160b15f97837a0ead130b29e4bfd7d7d58157bc
GoetzDeBouville <count.of.tuscan@gmail.com>
2025-11-05 21:14:39 +0300

    создает клиента для запросов на binance

---
+++ app/src/main/java/com/sample/ktorsample/core/data/models/di/NetworkModule.kt
@@ -0,0 +1,30 @@
+package com.sample.ktorsample.core.data.models.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+
+/**
+ * DO #10
+ *
+ * Создаем и пробрасываем в зависимости клиента
+ * 
+ * */
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    /**
+     * Для удобства всегда лучше создавать Qualifier для каждого клиента и его baseUrl.
+     * В Текущем примере ограничимся упрощенным вариантом.
+     *
+     * */
+    @Provides
+    fun provideBinanceClient(
+        factory: HttpClientFactory,
+        baseUrl: String = "https://data-api.binance.vision/api/v3/"
+    ): HttpClient {
+        return factory.create(baseUrl)
+    }
+}
